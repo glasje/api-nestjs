898293af2b7e4c3318634de0148c31e7012431ac
jesus15jimenez <jesus15jimenez@gmail.com>
2023-09-25 21:26:17 -0300

    add api rest pet

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configOptions } from './config/config-options';
+import { PetModule } from './pet/pet.module';
 @Module({
   imports: [
     ConfigModule.forRoot(configOptions),
@@ -18,9 +19,10 @@ import { configOptions } from './config/config-options';
         };
       },
       inject: [ConfigService],
-    }),],
+    }),
+    PetModule,
+  ],
   controllers: [AppController],
-  providers: [ConfigModule,
-    AppService],
+  providers: [ConfigModule, AppService],
 })
 export class AppModule {}
